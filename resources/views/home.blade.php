9596ade2e499ffdb568873a009f289d8f4a7e955
Taniel123 <tanielmaristela28@gmail.com>
2025-03-16 14:20:18 +0800

    hello

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 <div class="text-center">
-    <h1 class="text-4xl font-bold">Welcome to My Portfolio</h1>
+    <h1 class="text-4xl font-bold">Welcoe to My Portfolio</h1>
     <p class="mt-4 text-lg text-gray-600">I am a web developer passionate about building modern web applications.</p>
     <a href="/projects" class="mt-6 inline-block px-6 py-2 bg-blue-500 text-white rounded">View My Work</a>
 </div>
