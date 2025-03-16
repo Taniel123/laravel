55b8a0a28785fbf7cb9d6116994083710b2ece4b
Taniel123 <tanielmaristela28@gmail.com>
2025-03-16 14:03:44 +0800

    first commit

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('content')
+<h1 class="text-3xl font-bold">Contact Me</h1>
+<form action="#" class="mt-6">
+    <input type="text" placeholder="Your Name" class="w-full p-3 border rounded mb-4">
+    <input type="email" placeholder="Your Email" class="w-full p-3 border rounded mb-4">
+    <textarea placeholder="Your Message" class="w-full p-3 border rounded mb-4"></textarea>
+    <button class="px-6 py-2 bg-blue-500 text-white rounded">Send Message</button>
+</form>
+@endsection
